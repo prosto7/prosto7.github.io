d3bc160ef3b2a9db299d22b9bd3783db5174fa0b
[soft-lab-web] <[roman1199@mail.ru]>
2022-06-02 01:53:25 +0700

    first commit

---
+++ precache-manifest.de3ed1f31fbfacc58cbec25782b3625a.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb4e699cc935507c6f4d3cf7a46c709",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a81ee47d94fbdb8d133a",
+    "url": "/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "18bd6dbb8379c69d7749",
+    "url": "/static/css/main.b60a35eb.chunk.css"
+  },
+  {
+    "revision": "a81ee47d94fbdb8d133a",
+    "url": "/static/js/2.521c7806.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.521c7806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18bd6dbb8379c69d7749",
+    "url": "/static/js/main.526dab8a.chunk.js"
+  },
+  {
+    "revision": "fb1ac4abc2e66207ed63",
+    "url": "/static/js/runtime-main.403a1be9.js"
+  },
+  {
+    "revision": "57f8711d60f30fe8b031dab464eac12b",
+    "url": "/static/media/Alderaan.57f8711d.jpg"
+  },
+  {
+    "revision": "3b8cdd01a8247ded16023eae17035fe1",
+    "url": "/static/media/Bespin.3b8cdd01.png"
+  },
+  {
+    "revision": "462f554af47c1c35540c52f8f2fc44f7",
+    "url": "/static/media/Cerea.462f554a.png"
+  },
+  {
+    "revision": "f10cd5e34f3483498193610ce4f8aa32",
+    "url": "/static/media/Chandrila.f10cd5e3.png"
+  },
+  {
+    "revision": "0b470f24d307c87d8115d1f5fb19a60c",
+    "url": "/static/media/Concord_Dawn.0b470f24.png"
+  },
+  {
+    "revision": "3d0eb68fb2b6d4665bc9ff076bcaebd3",
+    "url": "/static/media/Corellia.3d0eb68f.png"
+  },
+  {
+    "revision": "d26ebbef08b256d8134359e71747519c",
+    "url": "/static/media/Coruscant.d26ebbef.jpg"
+  },
+  {
+    "revision": "58795608d50457c340e76d763e73c8d5",
+    "url": "/static/media/Dagobah.58795608.jpg"
+  },
+  {
+    "revision": "a9c7b1f33588f9ae14c5ace036508ec6",
+    "url": "/static/media/Endor.a9c7b1f3.png"
+  },
+  {
+    "revision": "0c6d163dc27f811a01fcd4c21c8d597f",
+    "url": "/static/media/Eriadu.0c6d163d.png"
+  },
+  {
+    "revision": "f5a2c987e9e7b24cf0fef8cd3523c2eb",
+    "url": "/static/media/Geonosis.f5a2c987.png"
+  },
+  {
+    "revision": "8bde1897afbbc8ed8ba214ed7c399031",
+    "url": "/static/media/HaruunKal.8bde1897.jpg"
+  },
+  {
+    "revision": "d7d13f36318182482c79ad0f8c16b011",
+    "url": "/static/media/Hoth.d7d13f36.png"
+  },
+  {
+    "revision": "17dc8092f3607915592b0b4096009b14",
+    "url": "/static/media/Mirial.17dc8092.jpg"
+  },
+  {
+    "revision": "f89edf272e8f592de0a8bc6189e80e3b",
+    "url": "/static/media/Mon Cala.f89edf27.png"
+  },
+  {
+    "revision": "9b6141b673bff2b515e5223d8da0886f",
+    "url": "/static/media/Mustafar.9b6141b6.png"
+  },
+  {
+    "revision": "9f255c0efbda5b6a050906d876466086",
+    "url": "/static/media/Naboo.9f255c0e.png"
+  },
+  {
+    "revision": "260245ecf139b5a8afcfe3158a15552c",
+    "url": "/static/media/Rodia.260245ec.png"
+  },
+  {
+    "revision": "7bb6797efd6477135f478624756f8306",
+    "url": "/static/media/Serenno.7bb6797e.png"
+  },
+  {
+    "revision": "6159ac524c56a89c57b0ad787e117faa",
+    "url": "/static/media/Sullust.6159ac52.png"
+  },
+  {
+    "revision": "985340c4a35b86201b55c247a5f158a6",
+    "url": "/static/media/Tatooine.985340c4.png"
+  },
+  {
+    "revision": "140755557766d8168efbdc9e17eab453",
+    "url": "/static/media/Tund.14075555.jpg"
+  },
+  {
+    "revision": "c2a4e1c9738585aa14386f7ffada5eef",
+    "url": "/static/media/Utapau.c2a4e1c9.png"
+  },
+  {
+    "revision": "637eb1b118a139c1f54bb3afa42da852",
+    "url": "/static/media/Yavin IV.637eb1b1.jpg"
+  }
+]);
